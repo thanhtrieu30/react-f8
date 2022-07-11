21121404244cada1c200f557eac746a270f49b12
Thanh Trieu <zpeng2k@gmail.com>
2022-07-11 10:34:10 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from '../Sidebar/Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('warpper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
